eff8242f2d5cc07b868e7af1c12b967d3885a832
Bailey Franklin <baileydfranklin@gmail.com>
2023-03-11 14:41:19 -0700

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
+
 const express = require ('express')
 const app = express ()
 
-app.get('/', (req, res) => {
-    res.send('Hellow World!')
+// app.get('/', (req, res) => {
+//     res.send('Hellow World!')
+// })
+
+app.get('*', (req, res) => {
+    res.status(404).send(`
+        <body>
+            <h1>404 Page</h1>
+            <details>
+                <summary><b>What does this mean?</b></summary>
+                Page does not exist. Please return to the previous page!
+            </details>
+        </body>
+    `)
 })
 
-app.listen(3000)
-
+app.listen(process.env.PORT)
