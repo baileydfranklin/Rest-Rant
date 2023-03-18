9001e93fb61e6a1fd7227a72d639c841acc9a978
Bailey Franklin <baileydfranklin@gmail.com>
2023-03-18 16:16:59 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,31 +1,31 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-  })
-  
-
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Another One Bites The Crust',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Italian',
-        pic: '/images/Photo3.avif'
-    }, {
-        name: 'Excuse My French Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery, Breakfast',
-        pic: '/images/Photo4.avif'
-    }]
+    let places = []
 
     res.render('places/index', { places })
 })
 
 module.exports = router
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'https://images.unsplash.com/photo-1458819714733-e5ab3d536722?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGdlbmVyYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+
+    places.push(req.body)
+    res.redirect('/places')
+  })
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Another One Bites The Crust',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Italian',
+    pic: '/images/Photo3.avif'
+}, {
+    name: 'Excuse My French Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery, Breakfast',
+    pic: '/images/Photo4.avif'
+}]
