03c9dd7b9784a4f761099aea9389d26b9ab8971a
Bailey Franklin <baileydfranklin@gmail.com>
2023-03-18 19:40:30 -0700

    Delete functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,29 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show(data) {
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
+            <main>
+                <h1>{data.place.name}</h1>
+                <br />
+                <h2>Rating</h2>
+                <p>Not Rated</p>
+                <br />
+                <br />
+                <h2>Comments</h2>
+                <p>No comments yet!</p>
+                <br />
+                <br />
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+            </main>
         </Def>
     )
 }
